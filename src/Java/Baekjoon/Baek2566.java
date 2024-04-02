82d3f02c4c4bbfcafe644b1090763aa5f7c7146d
dongha0312 <rsj1001@gmail.com>
2024-04-02 21:55:35 +0900

    prob.2566 fix

--- src/Java/Baekjoon/Baek2566.java
+++ src/Java/Baekjoon/Baek2566.java
@@ -6,20 +6,19 @@ public class Baek2566 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i,j;
         int max = 0;
         int x = 1, y = 1;
 
         int arr[][] = new int[9][9];
 
-        for(i=0;i<9;i++) {
-            for(j=0;j<9;j++) {
+        for(int i=0;i<9;i++) {
+            for(int j=0;j<9;j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        for(i=0;i<9;i++) {
-            for(j=0;j<9;j++) {
+        for(int i=0;i<9;i++) {
+            for(int j=0;j<9;j++) {
                 if(arr[i][j]>max) {
                     max = arr[i][j];
                     x = i+1;

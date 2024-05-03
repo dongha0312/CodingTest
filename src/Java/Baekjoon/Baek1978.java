517c7371dff9e449afb362e36e931b73c8d9d8f5
dongha0312 <rsj1001@gmail.com>
2024-05-03 21:07:22 +0900

    cote 5/3

--- src/Java/Baekjoon/Baek1978.java
+++ src/Java/Baekjoon/Baek1978.java
@@ -3,6 +3,8 @@ package Java.Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Baek1978 {
@@ -10,8 +12,20 @@ public class Baek1978 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-
+        int count = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
+        for(int i = 0; i < n; i++){
+            int isPrime = Integer.parseInt(st.nextToken());
+            for(int p=2; p<=isPrime; p++){
+                if(p == isPrime){
+                    count++;
+                }
+                if(isPrime % p == 0){
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
